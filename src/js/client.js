222123f90e20e8ee92047ed97dc8e69548969b15
Anil Rayamajhi <anilrm07@gmail.com>
2016-11-20 22:17:19 -0800

    backup

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Layout from "./components/Layout";
+class Layout extends React.Component {
+  render(){
+    return (
+      <h1>Ganey ko H1 </h1>
+    );
+  }
+}
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
