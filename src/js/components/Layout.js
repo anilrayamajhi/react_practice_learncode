93bcf60b8168c2e97798f67c2b600def88591a62
Anil Rayamajhi <anilrm07@gmail.com>
2016-11-21 18:16:52 -0800

    state example

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,12 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Header from "./Header.js"
+import Header from "./Header.js";
+import Footer from "./Footer.js";
 
 export default class Layout extends React.Component {
+  constructor(){
+    super();
+    this.state = {name:"Ganey"}
+  }
+
   render(){
+    setTimeout(() =>
+    {this.setState({name: "noway"})}, 1000
+  )
+
+    // var list=[<Header />, <Footer />];
+
     return (
-      <h1><Header/></h1>
-    );
+      // <h3>{list}</h3>
+      <h3>
+        {this.state.name}
+        <Header />
+        <Footer />
+      </h3>
+    )
   }
 }
